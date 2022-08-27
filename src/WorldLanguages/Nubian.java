c837e89522d48a4c11afa80c098357b5dad0e623
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-26 18:53:57 -0600

    Add Nubian class extending Language parent class. Add constructor to Nubian class

---
+++ src/WorldLanguages/Nubian.java
@@ -0,0 +1,7 @@
+package WorldLanguages;
+
+class Nubian extends Language{
+    public Nubian(String languageName, int speakers) {
+        super(languageName, speakers, "Eastern Africa", "verb-object-subject");
+    }
+}
