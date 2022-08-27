a08e6317ca9ba3fd36896edbbd96907695a3d487
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-26 18:37:20 -0600

    Add constructor method with four input parameters corresponding to each of the four instance variables

--- src/WorldLanguages/Language.java
+++ src/WorldLanguages/Language.java
@@ -5,6 +5,13 @@ public class Language {
     protected int numSpeakers;
     protected String regionSpoken;
     protected String wordOrder;
+
+    public Language(String langName, int speakers, String region, String wOrder) {
+        this.name = langName;
+        this.numSpeakers = speakers;
+        this.regionSpoken = region;
+        this.wordOrder = wOrder;
+    }
     public static void main(String[] args) {
 
     }
