e8d57453f382406a498bf42e51e87a94ec131e8c
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-26 20:13:12 -0600

    Add if statement to SinoTibetan class to handle Chinese languages' different wordOrder

--- src/WorldLanguages/SinoTibetan.java
+++ src/WorldLanguages/SinoTibetan.java
@@ -3,6 +3,8 @@ package WorldLanguages;
 class SinoTibetan extends Language{
     public SinoTibetan(String nameLanguage, int speakers) {
         super(nameLanguage, speakers, "Asia", "Subject, object, verb");
-
+        if (nameLanguage.contains("Chinese")) {
+            this.wordOrder = "Subject, verb, object";
+        }
     }
 }
