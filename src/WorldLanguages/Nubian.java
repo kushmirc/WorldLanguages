9856e00a6aadc3da90ec180da8de55f59bb6bae8
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-26 19:03:55 -0600

    Add getInfo() method to the Nubian class with an Override

--- src/WorldLanguages/Nubian.java
+++ src/WorldLanguages/Nubian.java
@@ -1,7 +1,14 @@
 package WorldLanguages;
 
 class Nubian extends Language{
+
     public Nubian(String languageName, int speakers) {
         super(languageName, speakers, "Eastern Africa", "subject-object-verb");
+    }
+    @Override
+    public void getInfo() {
+        System.out.println(this.name + " is spoken by " + this.numSpeakers + " people mainly in " + this.regionSpoken + ".");
+        System.out.println("The language follows the word order: " + wordOrder);
+        System.out.println("Fun fact: " + this.name + "-speakers were employed by Egypt as code talkers");
     }
 }
