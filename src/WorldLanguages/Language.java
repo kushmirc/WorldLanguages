1c287a5a6935d245eb91de7ae4ac33940c55cbc1
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-26 18:34:57 -0600

    Declare instance variables: name, numSpeakers, regionSpoken, wordOrder

--- src/WorldLanguages/Language.java
+++ src/WorldLanguages/Language.java
@@ -1,7 +1,10 @@
 package WorldLanguages;
 
 public class Language {
-
+    protected String name;
+    protected int numSpeakers;
+    protected String regionSpoken;
+    protected String wordOrder;
     public static void main(String[] args) {
 
     }
